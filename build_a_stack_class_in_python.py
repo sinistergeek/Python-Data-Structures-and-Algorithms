e4fb5ddd4e875e4204ce7749943d14acebce829b
sinister geek <susanbhattraidon@gmail.com>
2023-08-08 19:10:30 +0545

    Add files via upload

---
+++ build_a_stack_class_in_python.py
@@ -0,0 +1,43 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+
+    #    return len(self.items) == 0
+        return not self.items
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+
+if __name__ == "__main__":
+    s = Stack()
+    print(s)
+    print(s.is_empty())
+    s.push(23)
+    print(s)
+    s.push(8)
+    s.push(23)
+    s.push(123)
+    print(s)
+    print(s.pop())
+    print(s)
+    print(s.peek())
+    print(s.size())
+
+
+
+
+
